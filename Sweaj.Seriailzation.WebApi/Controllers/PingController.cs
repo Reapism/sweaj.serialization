c9fa37af7afd6a610355fc6fcebb9f4361183245
Reapism <reapsprgm@gmail.com>
2020-12-07 22:40:14 -0500

    properly add ping controller

---
+++ Sweaj.Seriailzation.WebApi/Controllers/PingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Sweaj.Seriailzation.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<PingController> _logger;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
