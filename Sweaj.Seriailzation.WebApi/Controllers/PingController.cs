a86360b766da4f3405dc61a7886689a9b84f1f4e
Reapism <reapsprgm@gmail.com>
2020-12-07 22:31:32 -0500

    Ping Controller webapi

---
+++ Sweaj.Seriailzation.WebApi/Controllers/PingController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sweaj.Seriailzation.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        public PingController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok("Welcome to the Serialization WebAPI. This is the default start location of the project. ");
+        }
+    }
+}
