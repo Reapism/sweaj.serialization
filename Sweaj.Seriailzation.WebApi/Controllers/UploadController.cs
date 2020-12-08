776386be5c014871a567962345397f1608beedf3
Reapism <reapsprgm@gmail.com>
2020-12-07 23:33:43 -0500

    Add video defaulter

---
+++ Sweaj.Seriailzation.WebApi/Controllers/UploadController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sweaj.Seriailzation.WebApi.Services;
+using Sweaj.Serialization.Data;
+using Sweaj.Serialization.Data.Models;
+
+namespace Sweaj.Seriailzation.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : ControllerBase
+    {
+
+        private readonly VideoContext context;
+        private readonly VideoDefaulterService videoDefaulter;
+
+        public UploadController(VideoContext context, VideoDefaulterService videoDefaulter)
+        {
+            this.context = context;
+            this.videoDefaulter = videoDefaulter;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<VideoMetadata>> Get(Guid id)
+        {
+            var model = await context.VideoMetadatas.FirstOrDefaultAsync(e => e.Id == id);
+            if (model is null)
+            {
+                return null;
+            }
+            return model;
+        }
+
+        // Create
+        [HttpPost]
+        public async Task<ActionResult> Post(VideoMetadata[] models)
+        {
+            if (models is null || models.Length == 0)
+            {
+                return NoContent();
+            }
+
+            var modelsList = models.ToList();
+            videoDefaulter.ApplyVideoDefaults(modelsList);
+
+            await context.VideoMetadatas.AddRangeAsync(modelsList);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        // Update
+        [HttpPut]
+        public async Task<ActionResult> Put(Guid id, VideoMetadata metadata)
+        {
+            if (id != metadata.Id)
+                return Problem("Cannot modify the video identifier.", null, (int)HttpStatusCode.Conflict, "Problem");
+
+            var model = await context.VideoMetadatas.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (model is null)
+                return NotFound();
+
+            model = metadata;
+            context.VideoMetadatas.Update(model);
+
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var model = await context.VideoMetadatas.FirstOrDefaultAsync(e => e.Id == id);
+            if (model is null)
+                return NotFound();
+
+            context.VideoMetadatas.Remove(model);
+
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Sweaj.Seriailzation.WebApi/Services/VideoDefaulterService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sweaj.Serialization.Data.Models;
+
+namespace Sweaj.Seriailzation.WebApi.Services
+{
+    public class VideoDefaulterService
+    {
+        private static readonly Random Random = new Random();
+
+        public void ApplyVideoDefaults(List<VideoMetadata> models)
+        {
+            models.ForEach(e =>
+            {
+                if (e.Id == Guid.Empty)
+                    e.Id = Guid.NewGuid();
+                if (e.ViewCount != 0)
+                    e.ViewCount = 0;
+
+                if (e.ShortDescription.Length > 100)
+                    e.ShortDescription.Trim().Substring(0, 100);
+
+                e.Language = "English";
+                e.PublishDate = DateTime.Now;
+                e.UploadDate = DateTime.Now;
+                e.ImageSeed = Random.Next().ToString();
+            });
+        }
+    }
+}
